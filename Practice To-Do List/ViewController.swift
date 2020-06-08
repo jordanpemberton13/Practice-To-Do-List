58f808e6dd918dd01ef53aad0a59c38e38a09f5f
Jordan <jordan13paige@gmail.com>
2020-06-08 18:53:51 -0400

    first push

---
+++ Practice To-Do List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice To-Do List
+//
+//  Created by Jordan Pemberton on 6/8/20.
+//  Copyright © 2020 Jordan Pemberton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
