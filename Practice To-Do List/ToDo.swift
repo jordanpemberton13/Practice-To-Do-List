256422b4160bb8d206e73f9510e9a5428b309bd0
Jordan <jordan13paige@gmail.com>
2020-06-08 18:52:47 -0400

    committing iteration 1

---
+++ Practice To-Do List/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  Practice To-Do List
+//
+//  Created by Jordan Pemberton on 6/8/20.
+//  Copyright © 2020 Jordan Pemberton. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
